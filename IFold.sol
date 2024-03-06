c60cf0cd5233f8b036a2def0c0817d0793d942d5
Labninety1 <143681184+labninety1@users.noreply.github.com>
2024-03-06 07:38:13 -0500

    Update IFold.sol

--- IFold.sol
+++ IFold.sol
@@ -2,6 +2,15 @@
 
 pragma solidity ^0.8.4;
 
-interface IFoldAdmin {
-    function depositLevelClaimedFold(uint256 _amount) external;
+interface IFold {
+    function approve(address _spender, uint _amount) external;
+    function mint(uint _amount) external;
+    function balanceOf(address who) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
 }
